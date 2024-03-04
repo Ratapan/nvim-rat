6575aa0f086936e8edc7bb74a97cab0b571b3642
Ratapan <javier2000asr@gmail.com>
2024-03-03 21:56:32 -0300

    feat screenshot

--- lua/plugins/screenShot.lua
+++ lua/plugins/screenShot.lua
@@ -1,17 +1,23 @@
 return {
   {
-    "mistricky/codesnap.nvim",
-    lazy = "true",
-    build = "make",
-    cmd = "CodeSnapPreviewOn",
-    opts = {
-      mac_window_bar = false,
-      opacity = true,
-      watermark = "Ratapan",
-      watermark_font_family = "Nimbus Mono",
-    },
-    config = function(_, opts)
-      require("codesnap").setup(opts)
+    -- Tener NotoColorEmoji.ttf
+    -- Instalar silicon $ sudo pacman -S silicon
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        font = "JetBrainsMono-Regular=34;NotoColorEmoji=34",
+        theme = "gruvbox-dark",
+        background = "#FFF7EB",
+        background_image = nil,
+        window_title = function()
+          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+        end,
+        output = function()
+          return "~/Pictures/Code/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+        end,
+      })
     end,
   },
 }
